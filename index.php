fef0538adc5b8ecc9876c11b0ef8bfdc2755ce25
Avinash M G <hatman290@gmail.com>
2019-01-27 19:19:31 +0530

    Setup File Made
    Added automated creation of the Business table

--- index.php
+++ index.php
@@ -8,6 +8,11 @@ if(isset($_GET['dir'])){
 
   if($request[0]!=null){
 
+    if($request[0]=="setup"){
+      include("./config/setup.php");
+    }
+
+
     if($request[0]=="login"){
       include("./login.html");
     }
@@ -15,12 +20,12 @@ if(isset($_GET['dir'])){
     elseif($request[0]=="forms"){
       include("./pages/forms-page.php");
     }
-    
+
 
     elseif($request[0]=="breg"){
       include("./pages/b-registration-page.php");
     }
-    
+
     else{
       include("./pages/404-page.php");
     }
