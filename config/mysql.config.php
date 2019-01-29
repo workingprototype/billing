34e29b4cc4fed06a88f3ac849a383d3c126a3cc6
Adarsh <adarshcool97@gmail.com>
2019-01-30 00:39:02 +0530

    config folder comment done

--- config/mysql.config.php
+++ config/mysql.config.php
@@ -1,6 +1,9 @@
+//
+* This file contains the database config of the application.
+*/
 <?php
-define('SQL_HOST', 'localhost');
-define('SQL_USERNAME', 'john_constantine');
-define('SQL_PASSWORD', 'imbatmanbitch');
-define('SQL_DBN', 'smcg_database');
+define('SQL_HOST', 'localhost'); //SQL Server. Its always at localhost. DONT CHANGE
+define('SQL_USERNAME', 'john_constantine'); // Your Database Username
+define('SQL_PASSWORD', 'imbatmanbitch'); // Your Database Password
+define('SQL_DBN', 'smcg_database');   //Your Database Name
 ?>
