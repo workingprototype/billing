363f115543b7891c4ab80a5f704986112d573855
Adarsh <adarshcool97@gmail.com>
2019-02-02 01:25:34 +0530

    Revert "SQL Setup update."
    This reverts commit 2b31928275fdf5a54849e2216ae8c21c87da01f1.

--- config/setup.php
+++ config/setup.php
@@ -16,53 +16,22 @@ if ($conn->query($sql) === TRUE) {
 
 $conn->close();
 $conn = new mysqli(SQL_HOST, SQL_USERNAME, SQL_PASSWORD, SQL_DBN);
-
-$sql = "CREATE TABLE IF NOT EXISTS `users`
+/**
+$sql = "CREATE TABLE IF NOT EXISTS `Users`
 ( `id` INT(4) NOT NULL AUTO_INCREMENT ,
 `fullname` VARCHAR(50) NOT NULL ,
 `username` VARCHAR(50) NOT NULL ,
-`passwordhash` BINARY(64) NOT NULL ,
+`password` VARCHAR(50) NOT NULL ,
 `status` INT(2) NOT NULL DEFAULT '0' ,
 PRIMARY KEY (`id`))
 ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci;";
 
 if ($conn->query($sql) === TRUE) {
-    echo "Table users created successfully";
+    echo "Table Users created successfully";
 } else {
     echo "Error creating table: " . $conn->error;
 }
-
-
-
-$sql = "CREATE TABLE IF NOT EXISTS `products`
-(  `pid` int(10) NOT NULL AUTO_INCREMENT,
-  `pname` varchar(500) NOT NULL,
-  `pbrand` varchar(100) NOT NULL,
-  `punit` varchar(50) NOT NULL,
-  `pcategory` varchar(200) NOT NULL,
-  `psubcategory` varchar(200) NOT NULL,
-  `psku` varchar(5000) NOT NULL,
-  `pquantity` varchar(100) NOT NULL,
-  `pimage` varchar(2083) NOT NULL,
-  `pweight` varchar(500) NOT NULL,
-  `ptaxapplicable` varchar(500) NOT NULL,
-  `ptaxtype` varchar(500) NOT NULL,
-  `ptype` varchar(500) NOT NULL,
-  `pamountexcludingtax` varchar(500) NOT NULL,
-  `pamountincludingtax` varchar(500) NOT NULL,
-  `pmarginamount` varchar(500) NOT NULL,
-  `psellingprice` varchar(500) NOT NULL,
-  `ptimestamp` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY (`pid`))
- ENGINE=InnoDB DEFAULT CHARSET=latin1;";
-
-if ($conn->query($sql) === TRUE) {
-    echo "<br>Table products created successfully";
-} else {
-    echo "Error creating table: " . $conn->error;
-}
-
-
+*/
 
 $sql = "CREATE TABLE IF NOT EXISTS `business`
 ( `id` INT NOT NULL AUTO_INCREMENT ,
@@ -89,7 +58,7 @@ PRIMARY KEY (`id`))
 ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci;";
 
 if ($conn->query($sql) === TRUE) {
-    echo "<br>Table business created successfully";
+    echo "<br>Table Business created successfully";
 } else {
     echo "Error creating table: " . $conn->error;
 }
