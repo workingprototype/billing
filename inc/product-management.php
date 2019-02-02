10cf211ff7ed3bab2a518f9a0b150112c20ac956
Adarsh <adarshcool97@gmail.com>
2019-02-02 23:07:59 +0530

    clean

--- inc/product-management.php
+++ inc/product-management.php
@@ -112,23 +112,22 @@ console.log(document.getElementById('pksu').value); //pass the value to the pksu
         $("div#taxshower").hide();
     }
 });							</script>
-							<div class="form-group">
-								<div id="taxshower">
-									<label for="ptaxtype" class="control-label">Tax Type:</label>
-									<div id="divtaxtype">
-										<select class="form-control" name="cgstbrowser" onchange="if(this.options[this.selectedIndex].value=='customOption'){toggleField(this,this.nextSibling); this.selectedIndex='0';}">
+
+		<div class="form-group">
+		<div id="taxshower">
+		<label for="ptaxtype" class="control-label">Tax Type:</label>
+		<div id="divtaxtype">
+	<select class="form-control" name="cgstbrowser" onchange="if(this.options[this.selectedIndex].value=='customOption'){toggleField(this,this.nextSibling); this.selectedIndex='0';}">
 			<option value="5"> 5 </option>
-			<option value="5"> 10 </option>
-			<option value="5"> 12 </option>
-			<option value="5"> 18 </option>
-			<option value="5"> 20 </option>
-			<option value="5"> 28 </option>
+			<option value="10"> 10 </option>
+			<option value="12"> 12 </option>
+			<option value="18"> 18 </option>
+			<option value="20"> 20 </option>
+			<option value="28"> 28 </option>
 			<option value="customOption"> [Enter a custom tax amount] </option></select><input class="form-control" name="cgstbrowser" style="display:none;" disabled="disabled" onblur="if(this.value==''){toggleField(this,this.previousSibling);}">
-			</div>
-
-								</div>
-
-						</div>
+</div>
+</div>
+</div>
 
 						<div class="form-group">
 							<label for="psku" class="control-label">Profit Margin:</label>
