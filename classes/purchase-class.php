4d28164209ac15ad2b924a63271e837126ff0fd8
Avinash M G <hatman290@gmail.com>
2019-02-10 09:51:38 +0530

    Purchase JS added

--- classes/purchase-class.php
+++ classes/purchase-class.php
@@ -12,6 +12,10 @@ class Purchase
     {
         return $this->head;
     }
+    public function script()
+    {
+        return $this->script;
+    }
 
 // Initializing the Return Property
     private $ret = "<form>
@@ -29,17 +33,41 @@ class Purchase
       </tr>
     </thead>
     <tbody>
-      <tr>
-      <td><input class='form-control lezz'></td>        
+      <tr class='bg-blue'>
+      <td><input id='product_id' class='form-control lezz'></td>        
         <td>
-      <input class=\"form-control\" placeholder=\"Product Name or Code\">
+      <input id='product_name' class=\"form-control\" onkeypress=\"return submit_name(event)\" placeholder=\"Product Name or Code\">
       </td>
-        <td><input class='form-control lezz'></td>
-        <td><input class='form-control lezz'></td>
+        <td><input id='product_q' class='form-control lezz'></td>
+        <td><input id='product_c' class='form-control lezz'></td>
+      </tr>
+      <tr id='last_row'>
       </tr>
     </tbody>
   </table>
-  </form>"; 
+  </form>";
+    private $script = "
+    <script>
+      function submit_name(e) {
+        if (e.keyCode == 13) {
+          var pname = document.getElementById(\"product_name\").value;
+          var pid = document.getElementById(\"product_id\").value;
+          var pquantity = document.getElementById(\"product_q\").value;
+          var pcost = document.getElementById(\"product_c\").value;
+          add_row(pname,pid,pquantity,pcost);
+        }
+      }
+      function add_row(a,b,c,d){
+        var last_row = document.getElementById('last_row');
+        last_row.outerHTML =\"<tr id='last_row'>\
+        </tr>\
+        <tr><td>\"+b+\"</td><td>\"+a+\"</td> \
+          <td>\"+c+\"</td>\
+          <td>\"+d+\"</td></tr>\";
+      }
+    </script>
+    
+    ";
     private $head = "<style>
     .lezz{
         width:100px;
