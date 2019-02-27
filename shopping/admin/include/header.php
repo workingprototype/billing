8aad46e0a11369041467bf302adbf2cf102690e9
A <adarshcool97@gmail.com>
2019-02-27 17:33:14 +0530

    clean

--- shopping/admin/include/header.php
+++ shopping/admin/include/header.php
@@ -5,7 +5,7 @@
 					<i class="icon-reorder shaded"></i>
 				</a>
 
-			  	<a class="brand" href="index.html">
+			  	<a class="brand" href="../../../billing/breg">
 			  		FMCG Shop | Admin
 			  	</a>
 
