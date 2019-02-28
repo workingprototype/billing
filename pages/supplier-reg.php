7c1d2d3f431963b4f45c01498ef0a994cce8ec1f
Avinash M G <hatman290@gmail.com>
2019-03-01 01:35:42 +0530

    Fix

--- pages/supplier-reg.php
+++ pages/supplier-reg.php
@@ -1,4 +1,17 @@
 <?php
+require_once "./classes/page-class.php";
+require_once "./classes/sidebar-class.php";
+require_once "./classes/top-navigation-class.php";
+require_once "./classes/footer-class.php";
+$page = new Page;
+$sidebar = new Sidebar;
+$footer = new Footer;
+$navbar = new TopNav;
+$page->var['navbar']=$navbar->echo();
+$page->var['sidebar']=$sidebar->echo();
+$page->var['footer']=$footer->echo();
+$page->var['title']="Create New Supplier Account";
+
 session_start();
 error_reporting(0);
 include('./config/config.php');
@@ -21,27 +34,8 @@ else{
 echo "<script>alert('Not registered. Something went wrong.');</script>";
 }
 }
-?>
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<!-- Meta -->
-		<meta charset="utf-8">
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-		<meta name="description" content="">
-		<meta name="author" content="">
-	    <meta name="keywords" content="register supplier">
-	    <meta name="robots" content="all">
+$page->var['content']='
 
-	    <title>Register Supplier </title>
-
-	    <!-- Bootstrap Core CSS -->
-	    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-
-
-		<!-- Favicon -->
-		<link rel="shortcut icon" href="assets/images/favicon.ico">
 <script type="text/javascript">
 function valid()
 {
@@ -54,12 +48,12 @@ return false;
 return true;
 }
 </script>
-			<script>
+<script>
 function userAvailability() {
 $("#loaderIcon").show();
 jQuery.ajax({
 url: "check_availability.php",
-data:'email='+$("#email").val(),
+data:\'email=\'+$("#email").val(),
 type: "POST",
 success:function(data){
 $("#user-availability-status1").html(data);
@@ -69,44 +63,36 @@ error:function (){}
 });
 }
 </script>
-	</head>
 <!-- create a new account -->
-<div class="col-md-6 col-sm-6 create-new-account">
-	<h4 class="checkout-subtitle">Create a new Supplier Account</h4>
-	<form class="register-form outer-top-xs" role="form" method="post" name="register" onSubmit="return valid();">
-    <span style="color:red;" >
-  <?php
-  echo htmlentities($_SESSION['errmsg']);
-  ?>
-  <?php
-  echo htmlentities($_SESSION['errmsg']="");
-  ?>
-  	</span>
+<div  class="col-md-6 col-sm-6">
+	<form  class="register-form outer-top-xs" role="form" method="post" name="register" onSubmit="return valid();">
+    <span style="color:red;" >'.htmlentities($_SESSION['errmsg']).htmlentities($_SESSION['errmsg']="").'
+	  </span>
 <div class="form-group">
 	    	<label class="info-title" for="fullname">Full Name <span>*</span></label>
 	    	<input type="text" class="form-control unicase-form-control text-input" id="fullname" name="fullname" required="required">
-	  	</div>
+</div>
 
 
-		<div class="form-group">
+<div class="form-group">
 	    	<label class="info-title" for="exampleInputEmail2">Email Address <span>*</span></label>
 	    	<input type="email" class="form-control unicase-form-control text-input" id="email" onBlur="userAvailability()" name="emailid" required >
 	    	       <span id="user-availability-status1" style="font-size:12px;"></span>
-	  	</div>
+</div>
 
 <div class="form-group">
 	    	<label class="info-title" for="contactno">Contact No. <span>*</span></label>
 	    	<input type="text" class="form-control unicase-form-control text-input" id="contactno" name="contactno" maxlength="10" required >
-	  	</div>
+</div>
 <div class="form-group">
       	    	<label class="info-title" for="altcontactno">Alternative Contact No. <span>*</span></label>
       	    	<input type="text" class="form-control unicase-form-control text-input" id="altcontactno" name="altcontactno" maxlength="10" required >
-      </div>
+</div>
 
 <div class="form-group">
 	    	<label class="info-title" for="password">Password. <span>*</span></label>
 	    	<input type="password" class="form-control unicase-form-control text-input" id="password" name="password"  required >
-	  	</div>
+</div>
 
 <div class="form-group">
 	    	<label class="info-title" for="confirmpassword">Confirm Password. <span>*</span></label>
@@ -123,8 +109,12 @@ error:function (){}
 							</textarea>
 </div>
 
-	  	<button type="submit" name="submit" class="btn-upper btn btn-primary checkout-page-button" id="submit">Register Supplier</button>
+	<button type="submit" name="submit" class="btn-upper btn btn-primary checkout-page-button" id="submit">Register Supplier</button>
 	</form>
 
 </div>
-<!-- create a new account -->
+';
+
+$page->render();
+
+?>
