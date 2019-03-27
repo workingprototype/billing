63258e85744437e092aebe4803d9264d8d81bd44
Avinash M G <hatman290@gmail.com>
2019-03-28 00:51:36 +0530

    sales id problem fixed

--- pages/functions-all.php
+++ pages/functions-all.php
@@ -200,7 +200,7 @@ elseif($request[1]=="sales")
   $customer = $_POST['customer'];
   $invoice = (time()*100)+34;
   foreach ($data as $k => $v) {
-
+    $v[0]=explode("_",$v[0])[1];
     $val=[$v[1],$v[0],$v[3],$v[4],$v[6],$v[5],$v[8],$v[9],$v[10],$v[11],$v[12],$v[13],$v[14],$invoice,$v[2],$timestamp,$customer];
     $table="sales";
     $col= [
