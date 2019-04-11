88c0873408f58a0eadd490600ad7f12a5618928f
A <adarshcool97@gmail.com>
2019-04-11 23:57:34 +0530

    Database Update for Executive page. Do Setup before proceeding

--- pages/todays-orders.php
+++ pages/todays-orders.php
@@ -17,7 +17,7 @@ $currentTime = date( 'd-m-Y h:i:s A', time () );
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Admin| Pending Orders</title>
+	<title>Admin| Today's Orders</title>
 	<link type="text/css" href="./shopping/admin/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	<link type="text/css" href="./shopping/admin/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
 	<link type="text/css" href="./shopping/admin/css/theme.css" rel="stylesheet">
@@ -48,7 +48,7 @@ popUpWin = open(URLStr,'popUpWin', 'toolbar=no,location=no,directories=no,status
 
 	<div class="module">
 							<div class="module-head" width="160px;">
-								<h3>Pending Orders</h3>
+								<h3>Today's Orders</h3>
 							</div>
 							<div class="module-body table">
 	<?php if(isset($_GET['del']))
