de9161004fa0ae6a63f99d40951d12479c50a61b
A <adarshcool97@gmail.com>
2019-04-14 09:31:26 +0530

    Executive Add Payments autocomplete

--- pages/exec/add-payments.php
+++ pages/exec/add-payments.php
@@ -1,37 +1,19 @@
 
 <?php
-session_start();
-include('config/config.php');
-if(strlen($_SESSION['alogin'])==0)
-	{
-header('location:exec');
-}
-else{
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Admin| Payments</title>
-	<link type="text/css" href="./shopping/admin/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-	<link type="text/css" href="./shopping/admin/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
-	<link type="text/css" href="./shopping/admin/css/theme.css" rel="stylesheet">
-	<link type="text/css" href="./shopping/admin/images/icons/css/font-awesome.css" rel="stylesheet">
-	<link type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600' rel='stylesheet'>
-	<script>
+$content="
+<script>
 function duelist(a){
 	var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
     	if (this.readyState == 4 && this.status == 200) {
             console.log(this.responseText);
-			document.getElementById("due").innerHTML=this.responseText;
+			document.getElementById(\"due\").innerHTML=this.responseText;
         }
     };
-    xhttp.open("POST", "function/duelist ", true);
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send("id="+a);
+    xhttp.open(\"POST\", \"function/duelist \", true);
+    xhttp.setRequestHeader(\"Content-type\", \"application/x-www-form-urlencoded\");
+    xhttp.send(\"id=\"+a);
 	duelist2(a);
 }
 function duelist2(a){
@@ -39,101 +21,110 @@ function duelist2(a){
     xhttp.onreadystatechange = function() {
     	if (this.readyState == 4 && this.status == 200) {
             console.log(this.responseText);
-			document.getElementById("dueshow").innerHTML=this.responseText;
+			document.getElementById(\"dueshow\").innerHTML=this.responseText;
         }
     };
-    xhttp.open("POST", "function/duelist2", true);
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send("id="+a);
+    xhttp.open(\"POST\", \"function/duelist2\", true);
+    xhttp.setRequestHeader(\"Content-type\", \"application/x-www-form-urlencoded\");
+    xhttp.send(\"id=\"+a);
 }
+
+function autocompleted(id,value,supervalue){
+	document.getElementById(id).value= value;
+	document.getElementById('hidden_'+id).value= supervalue;
+	document.getElementById('drop_'+id).innerHTML='';
+	duelist(supervalue);
+  }
+  function autocompletex(value,a){
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+	  if (this.readyState == 4 && this.status == 200) {
+		document.getElementById('drop_'+a).innerHTML=this.responseText;
+	  }
+	};
+	xhttp.open(\"POST\", \"function/auto\"+a, true);
+	xhttp.setRequestHeader(\"Content-type\", \"application/x-www-form-urlencoded\");
+	xhttp.send('data='+value);
+  }
+
 </script>
-</head>
-<body>
+
+			<div style='margin:20px;' class=\"row\">
+
+			<div class=\"span9\">
+					<div class=\"content\">
+
+						<div class=\"module\">
+							<div class=\"module-body\">";
+
+if(isset($_POST['submit']))
+{
+ $content.="<div class=\"alert alert-success\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\">×</button>
+									<strong>Well done!</strong>";
+$content.=htmlentities($_SESSION['msg']);
+$content.=htmlentities($_SESSION['msg']="");
+
+$content.="</div>";
+}
+if(isset($_GET['del']))
+{
+	$content.= "<div class=\"alert alert-error\">
+	<button type=\"button\" class=\"close\" data-dismiss=\"alert\">×</button>
+	<strong>Oh snap!</strong> ".$_SESSION['delmsg'];
+	$_SESSION['delmsg']="";
+	$content.=	"</div>";
+}
+$content.="<br />
 
 
-	<div class="wrapper">
-		<div class="container">
-			<div class="row">
+			<form class=\"form-horizontal row-fluid\" action=\"./function/record_payment\" name=\"subcategory\" method=\"post\" >
 
-			<div class="span9">
-					<div class="content">
-
-						<div class="module">
-							<div class="module-head">
-								<h3>Add Payments</h3>
-							</div>
-							<div class="module-body">
-
-									<?php if(isset($_POST['submit']))
-{?>
-									<div class="alert alert-success">
-										<button type="button" class="close" data-dismiss="alert">×</button>
-									<strong>Well done!</strong>	<?php echo htmlentities($_SESSION['msg']);?><?php echo htmlentities($_SESSION['msg']="");?>
-									</div>
-<?php } ?>
-
-
-									<?php if(isset($_GET['del']))
-{?>
-									<div class="alert alert-error">
-										<button type="button" class="close" data-dismiss="alert">×</button>
-									<strong>Oh snap!</strong> 	<?php echo htmlentities($_SESSION['delmsg']);?><?php echo htmlentities($_SESSION['delmsg']="");?>
-									</div>
-<?php } ?>
-
-									<br />
-
-
-			<form class="form-horizontal row-fluid" action="./function/record_payment" name="subcategory" method="post" >
-
-<div class="control-group">
-<label class="control-label" for="basicinput">Payments</label>
-<div class="controls">
-<select  onchange="return duelist(this.value)" name="category" class="span8 tip" required>
-<option value="">Select Debtor</option>
-<?php $query=mysqli_query($con,"SELECT * FROM users ");
-while($row=mysqli_fetch_array($query))
-{?>
-
-<option value="<?php echo $row['id'];?>"><?php echo $row['name'];?></option>
-<?php } ?>
-</select>
+<div  class=\"control-group\">
+<label class=\"control-label\" for=\"basicinput\">Debtor</label>
+<div class=\"controls\">";
+$content.="
+<input style='visibility:hidden;position:absolute' placeholder='Start typing the name of the debtor' id='hidden_customer' class='form-control'>
+    <input id='customer' onkeyup='autocompletex(this.value,\"customer\")' class='form-control' autocomplete='chromeisnotnice'>
+    <div id='drop_customer' style='width:347px;background:#999;position:absolute;z-index:2'>
+    </div>
 </div>
 </div>
 
 
-<div class="control-group">
-<div class="controls">
+<div class=\"control-group\">
+<div class=\"controls\">
 
 <div id='dueshow'>
 </div>
 </div>
 
-</div><div class="control-group">
-<label class="control-label" for="basicinput">Invoice Number: </label>
-<div class="controls">
+</div><div class=\"control-group\">
+<label class=\"control-label\" for=\"basicinput\">Invoice Number: </label>
+<div class=\"controls\">
 
-<select name='invoice' id='due'>
+<select style='width:400px' name='invoice' id='due'>
 </select>
 </div>
 </div>
 
 
-<div class="control-group">
-<label class="control-label" for="basicinput">Payment Amount</label>
-<div class="controls">
-<input type="text" placeholder="Enter Payment Amount"  name="amountpaying" class="span8 tip" required>
+<div class=\"control-group\">
+<label class=\"control-label\" for=\"basicinput\">Payment Amount</label>
+<div class=\"controls\">
+<input type=\"text\" placeholder=\"Enter Payment Amount\"  name=\"amountpaying\" class=\"span8 tip\" required>
 </div>
 </div>
+</br> </br>
 
 
-
-	<div class="control-group">
-											<div class="controls">
-												<button type="submit" name="submit" class="btn"style="border-radius: 3px;color: #fff;
+	<div class=\"control-group\">
+											<div class=\"controls\">
+												<button type=\"submit\" name=\"submit\" class=\"btn\"style=\"border-radius: 3px;color: #fff;
 		background-color: #5cb85c;
-		border-color: #4cae4c;">Record Payment</button>
-													<button onclick="location.href = './pages/exec/index.php';"> Return Home </button>
+		border-color: #4cae4c;\">Record Payment</button>
+													<button onclick=\"location.href = '/billing/pages/exec/index.php';\" class=\"btn\"style=\"border-radius: 0.5px;color: white;
+			background-color: black;
+			border-color: white;\"> Return Home </button>
 											</div>
 										</div>
 									</form>
@@ -147,25 +138,10 @@ while($row=mysqli_fetch_array($query))
 
 					</div><!--/.content-->
 				</div><!--/.span9-->
-			</div>
-		</div><!--/.container-->
-	</div><!--/.wrapper-->
-
-
-
-	<script src="./shopping/admin/scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
-	<script src="./shopping/admin/scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
-	<script src="./shopping/admin/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-	<script src="./shopping/admin/scripts/flot/jquery.flot.js" type="text/javascript"></script>
-	<script src="./shopping/admin/scripts/datatables/jquery.dataTables.js"></script>
-	<script>
-		$(document).ready(function() {
-			$('.datatable-1').dataTable();
-			$('.dataTables_paginate').addClass("btn-group datatable-pagination");
-			$('.dataTables_paginate > a').wrapInner('<span />');
-			$('.dataTables_paginate > a:first-child').append('<i class="icon-chevron-left shaded"></i>');
-			$('.dataTables_paginate > a:last-child').append('<i class="icon-chevron-right shaded"></i>');
-		} );
-	</script>
-</body>
-<?php } ?>
+			</div>";
+require_once "./classes/page-class.php";
+$page = new Page;
+$page->var['content']=$content;
+$page->var['title']="Add Payments";
+$page->render();
+?>
