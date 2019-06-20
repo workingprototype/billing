96c99e92ef3d5688b52df50b38ee552d2a5f70f9
A <adarshcool97@gmail.com>
2019-06-20 12:56:22 +0530

    Added popup and ESC key event to pending, delivered and today's order

--- shopping/admin/todays-orders-expanded.php
+++ shopping/admin/todays-orders-expanded.php
@@ -96,7 +96,7 @@ $content.='
 											<td>'.htmlentities($row['quantity']).'</td>
 											<td>₹'.htmlentities($row['quantity']*$row['productprice']+$row['shippingcharge']).'</td>
 											<td>'.htmlentities($row['orderdate']).'</td>
-											<td>    <a href="updateorder.php?oid='.htmlentities($row['id']).'" title="Update order" target="_blank"><i class="icon-external-link"></i></a>
+											<td> <button id="myBtn" >Update Order</button>  
 											</td>
 											</tr>';
 $cnt=$cnt+1; }
@@ -112,6 +112,98 @@ $content.='
 			</div><!--/.container-->
 			</div>
 
+			<style>
+			body {font-family: Arial, Helvetica, sans-serif;}
+
+			/* The Modal (background) */
+			.modal {
+				display: none; /* Hidden by default */
+				position: fixed; /* Stay in place */
+				z-index: 1; /* Sit on top */
+				padding-top: 100px; /* Location of the box */
+				width: 100%; /* Full width */
+				height: 100%; /* Full height */
+				overflow: auto; /* Enable scroll if needed */
+				background-color: rgb(0,0,0); /* Fallback color */
+				background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+			}
+
+			/* Modal Content */
+			.modal-content {
+				background-color: #fefefe;
+				margin: auto;
+				padding: 20px;
+				border: 1px solid #888;
+				width: 80%;
+			}
+
+			/* The Close Button */
+			.close {
+				color: #aaaaaa;
+				float: right;
+				font-size: 28px;
+				font-weight: bold;
+			}
+
+			.close:hover,
+			.close:focus {
+				color: #000;
+				text-decoration: none;
+				cursor: pointer;
+			}
+			</style>
+
+
+
+			<!-- The Modal -->
+			<div id="myModal" class="modal">
+
+				<!-- Modal content -->
+				<div class="modal-content">
+					<span class="close">&times;</span>
+					<div id="myModal" class="full reveal" data-reveal></div>
+					<script>
+					$("#myBtn").on("click",function(e) {
+						e.preventDefault();
+
+
+						$("#myModal")
+							.html(\'<object width="50%" height="50%" type="text/html" align-items="center" data="updateorder.php?oid='.htmlentities($row['id']).'" ></object>\')
+							.foundation("open");
+
+					});
+					</script>
+				</div>
+
+			</div>
+
+			<script>
+			// Get the modal
+			var modal = document.getElementById("myModal");
+
+			// Get the button that opens the modal
+			var btn = document.getElementById("myBtn");
+
+			// Get the <span> element that closes the modal
+			var span = document.getElementsByClassName("close")[0];
+
+			// When the user clicks the button, open the modal
+			btn.onclick = function() {
+				modal.style.display = "block";
+			}
+
+			// When the user clicks on <span> (x), close the modal
+			span.onclick = function() {
+				modal.style.display = "none";
+			}
+
+			// When the user clicks anywhere outside of the modal, close it
+			window.onclick = function(event) {
+				if (event.target == modal) {
+					modal.style.display = "none";
+				}
+			}
+			</script>
 
 			<link type="text/css" href="images/icons/css/font-awesome.css" rel="stylesheet">
 			<script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
