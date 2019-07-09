49dc2693cfbc9daedbe8573f0c891d3824a1c563
A <adarshcool97@gmail.com>
2019-07-10 00:24:11 +0530

    hostinger username and password

--- shopping/admin/include/config.php
+++ shopping/admin/include/config.php
@@ -3,9 +3,9 @@ define('APP_NAME', 'FMCG Application');  //Name of the Application
 define('APP_TITLE', 'FMCG Demo'); // Name to show on the title bar of the browser
 define('APP_ROOT', 'https://insanelyelegant.com/demo/billing/');
 define('DB_SERVER','localhost');
-define('DB_USER','john_constantine');
-define('DB_PASS' ,'imbatmanbitch');
-define('DB_NAME', 'smcg_database');
+define('DB_USER','u306375126_john');
+define('DB_PASS' ,'password');
+define('DB_NAME', 'u306375126_bill');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
